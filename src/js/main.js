1cb23ea419d8f5fc8b1564ad5a24c7b7f9b96371
SharyginNikita <sharygin.n@yandex.ru>
2019-07-10 17:54:41 +0300

    add popups

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 import Swiper from 'swiper/dist/js/swiper.min.js';
 import popup from 'popup';
+import IMask from 'imask';
 
 let burger = document.querySelector('#header__menu-btn');
 let menu = document.querySelector('.header-nav');
@@ -37,6 +38,33 @@ let swiperMain = new Swiper('.section-swiper', {
     },
 });
 
+document.querySelector('.buttons-contact__button_paper-plane').addEventListener('click', _ => {
+
+    let communication = 
+
+        `
+
+<div class="popup">
+    <div class="popup__content">
+        <div class="popup__item popup__item_column"><label class="popup__label" for="input_1">Ваш EMail</label><input class="popup__input" type="text" placeholder="***@***" id="input_1" /></div>
+        <div class="popup__item popup__item_column"><label class="popup__label" for="input_2">Ваш вопрос</label><input class="popup__input" type="text" placeholder="Введите текст" id="input_2" /></div>
+        <div class="popup__item popup__item_column"><label class="popup__label" for="input_3">Можете прикрепить файл</label><input class="popup__input popup__input_file" type="file" id="input_3" multiple="multiple" /></div>
+        <div class="popup__item popup__item_row">
+            <div class="popup__checkbox-wrapper"><label class="checkbox-label"><input type="checkbox" /><span class="checkbox-custom rectangular"></span></label></div>
+            <div class="popup__agree">Я согласен на <a class="popup__agree-link" href="#">обработку персональных данных</a></div>
+        </div>
+        <div class="button button_send">Отправить заявку</div>
+    </div>
+</div>
+`
+    ;
+
+    popup.insertData(communication); 
+    popup.open();
+
+});
+
+
 document.querySelector('.buttons-contact__button_call').addEventListener('click', _ => {
 
     let communication = 
@@ -45,30 +73,57 @@ document.querySelector('.buttons-contact__button_call').addEventListener('click'
 
 <div class="popup">
     <div class="popup__content">
+        <div class="popup__item popup__item_column"><label class="popup__label" for="input_1">Ваш Телефон</label><input class="popup__input phone-mask" type="text" placeholder="+7 (***)  **-**-***" id="input_1" /></div>
         <div class="popup__item popup__item_column">
-            <div class="popup__title">Ваш EMail</div><input class="popup__input" type="text" placeholder="***@***" />
+            <div class="popup__label">Удобное время для звонка</div>
+            <div class="popup__row popup__row_between">
+                <div class="popup__select-wrapper">
+                    <div class="popup__select-text">с</div><select class="popup__select">
+                        <option>09.00</option>
+                        <option>10.00</option>
+                    </select>
+                </div>
+                <div class="popup__select-wrapper">
+                    <div class="popup__select-text">до</div><select class="popup__select">
+                        <option>09.00</option>
+                        <option>10.00</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="popup__item popup__item_column">
-            <div class="popup__title">Ваш вопрос</div><input class="popup__input" type="text" placeholder="Введите текст" />
-        </div>
-        <div class="popup__item popup__item_column">
-            <div class="popup__title">Можете прикрепить файл</div><input class="popup__input" type="file" />
-        </div>
-        <div class="popup__item popup__item_row"><input type="checkbox" />
-            <div class="popup__agree">Я согласен на обработку персональных данных</div>
+        <div class="popup__item popup__item_row">
+            <div class="popup__checkbox-wrapper"><label class="checkbox-label"><input type="checkbox" /><span class="checkbox-custom rectangular"></span></label></div>
+            <div class="popup__agree">Я согласен на <a class="popup__agree-link" href="#">обработку персональных данных</a></div>
         </div>
         <div class="button button_send">Отправить заявку</div>
     </div>
-</div>
+</div>`
+;
 
-    `
-
-    ;
-
-    popup.insertData(communication); 
+    popup.insertData(communication);
+    let mask = document.querySelector('.phone-mask');
+    let phoneMask = IMask(
+        mask, {
+            mask: '+{7}(000)000-00-00'
+        });
     popup.open();
 
 });
+
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 /*
 
 import yuor vue modules example
