08f5bf586e95e93f0c0fc0bab4e668dfd40d4532
SharyginNikita <sharygin.n@yandex.ru>
2019-07-10 13:06:02 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Swiper from 'swiper/dist/js/swiper.min.js';
+import popup from 'popup';
 
 let burger = document.querySelector('#header__menu-btn');
 let menu = document.querySelector('.header-nav');
@@ -35,8 +36,41 @@ let swiperMain = new Swiper('.section-swiper', {
         bulletActiveClass: 'section-swiper__bullet_active',
     },
 });
+
+document.querySelector('.buttons-contact__button_call').addEventListener('click', _ => {
+
+    let communication = 
+
+        `
+
+<div class="popup">
+    <div class="popup__content">
+        <div class="popup__item popup__item_column">
+            <div class="popup__title">Ваш EMail</div><input class="popup__input" type="text" placeholder="***@***" />
+        </div>
+        <div class="popup__item popup__item_column">
+            <div class="popup__title">Ваш вопрос</div><input class="popup__input" type="text" placeholder="Введите текст" />
+        </div>
+        <div class="popup__item popup__item_column">
+            <div class="popup__title">Можете прикрепить файл</div><input class="popup__input" type="file" />
+        </div>
+        <div class="popup__item popup__item_row"><input type="checkbox" />
+            <div class="popup__agree">Я согласен на обработку персональных данных</div>
+        </div>
+        <div class="button button_send">Отправить заявку</div>
+    </div>
+</div>
+
+    `
+
+    ;
+
+    popup.insertData(communication); 
+    popup.open();
+
+});
 /*
- 
+
 import yuor vue modules example
 
 import Module from '../widgets/module';
